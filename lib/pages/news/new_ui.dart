72700cb3c4f27c8178b937127c9e070dbdb58c7c
YMEZA97 <yasalberto@hotmail.com>
2024-08-13 00:05:47 -0600

    Pagina de Noticias 0.5

--- lib/pages/news/new_ui.dart
+++ lib/pages/news/new_ui.dart
@@ -21,9 +21,8 @@ class _NewUiState extends State<NewUi> {
         child:  Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Card(child: Cards(cardName: 'Noticia 1')),
-              Card(child: Cards(cardName: 'Noticia 2')),
-              Card(child: Cards(cardName: 'Noticia 3')),
+              Card(child: Cards(cardName: 'Noticia 1', cardImage: "https://store-images.s-microsoft.com/image/apps.36835.14492785218726113.9d2f0146-3acc-4079-9f6b-3299fae5a03a.5acf0d90-bd2c-4fe6-9c97-5316989382bb?q=90&w=256&h=256&mode=crop&format=jpg&background=%23464646",cardNew: "Hola es es la noticia",)),
+             
             ],
           ),
        
--- lib/pages/news/widget/cards.dart
+++ lib/pages/news/widget/cards.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
 
 class Cards extends StatelessWidget {
-  const Cards({super.key, required this.cardName});
+  const Cards({super.key, required this.cardName, required this.cardImage, required this.cardNew});
   final String cardName;
-
+final String cardImage;
+final String cardNew;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      height: 100,
-      child: Center(child: Text(cardName)),
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Ink.image(
+              image: NetworkImage(
+                cardImage,
+              ),
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 16,
+              right: 16,
+              left: 16,
+              child: Text(
+                cardName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.all(16).copyWith(bottom: 0),
+          child: Text(
+            cardNew,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+        ButtonBar(
+          alignment: MainAxisAlignment.start,
+          children: [
+           TextButton.icon(
+                icon: const Icon(Icons.visibility),
+                onPressed: () {                   
+                },
+                label: const Text(
+                  'Ver Noticia',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                ),
+              ),
+            ],
+        )
+      ],
     );
   }
 }
-
