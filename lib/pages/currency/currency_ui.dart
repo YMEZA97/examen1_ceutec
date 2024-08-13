054f246f647746cd5074f7148169dc629528c344
YMEZA97 <yasalberto@hotmail.com>
2024-08-13 02:02:24 -0600

    Pagina de Tasa de cambios 0.3 agregando logica

--- lib/pages/currency/currency_ui.dart
+++ lib/pages/currency/currency_ui.dart
@@ -7,30 +7,61 @@ class CurrencyUi extends StatefulWidget {
   State<CurrencyUi> createState() => _CurrencyUiState();
 }
 
+  
+
+
+
+
 class _CurrencyUiState extends State<CurrencyUi> {
+
+final TextEditingController controlador = TextEditingController();
+  String selectmoneda = 'USD';
+  double resultado = 0.0;
+  final double lempirasaDolar = 0.041;
+  final double dolaraLempiras = 24.3;  
+  final double lempirasaEuro = 0.034;   
+  final double euroaLempiras = 29.5;    
+
+ void convertidor() {
+    double input = double.tryParse(controlador.text) ?? 0.0;
+    setState(() {
+      if (selectmoneda == 'USD') {
+        resultado = input * dolaraLempiras;
+      } else if (selectmoneda == 'EUR') {
+        resultado = input * euroaLempiras;
+      } else if (selectmoneda == 'HNL A USD') {
+        resultado = input * lempirasaDolar;
+      } else if (selectmoneda == 'HNL A EUR') {
+        resultado = input * lempirasaEuro;
+      }
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tasa de Cambio'),
       ),
-      body: Padding(
+    body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(
-   
+            TextField(
+              controller: controlador,
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Ingrese la cantidad en Lempiras',
               ),
             ),
-            const SizedBox(height: 20),
+           const SizedBox(height: 20),
             DropdownButton<String>(
-           
-              items: <String>['USD', 'EUR', 'HNL a USD', 'HNL a EUR']
+              value: selectmoneda,
+              items: <String>['USD', 'EUR', 'HNL to USD', 'HNL to EUR']
                   .map((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
@@ -39,28 +70,19 @@ class _CurrencyUiState extends State<CurrencyUi> {
               }).toList(),
               onChanged: (newValue) {
                 setState(() {
-                
+                  selectmoneda = newValue!;
                 });
               },
             ),
-            const SizedBox(height: 20),
-          TextButton.icon(
-                  icon: const Icon(Icons.currency_exchange, size: 5),
-                  onPressed: () {
-                   
-                  },
-                  label: const Text(
-                    'Convertir',
-                    style: TextStyle(
-                      color: Color.fromARGB(0, 0, 0, 0),
-                    ),
-                  ),
-                ),
-
-            const SizedBox(height: 20),
-            const Text(
-              'Resultado:',
-              style: TextStyle(fontSize: 24),
+           const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: convertidor,
+              child: const Text('Convertir'),
+            ),
+           const SizedBox(height: 20),
+            Text(
+              'Resultado: $resultado',
+              style:const TextStyle(fontSize: 24),
             ),
           ],
         ),
